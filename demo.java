c579019cce40b076b0606f16ccc382310139afaa
Janine Jay <janinerjay@aol.com>
2017-10-10 19:48:48 -0400

    printed out janine

--- demo.java
+++ demo.java
@@ -4,5 +4,7 @@ public class demo {
     public static void main (String[] args) {
         System.out.println("Hello");
         out.println("hi");
+        
+        out.println("Janine");
     }
 }
